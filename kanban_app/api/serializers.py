9d8809d4a02b5d086c5491cec0b7af21013f8db5
Leugzim Rullani <leugzimrullani@outlook.com>
2025-06-14 19:12:34 +0200

    Returns a list of comments with id, creation timestamp, author full name, and content

--- kanban_app/api/serializers.py
+++ kanban_app/api/serializers.py
@@ -15,14 +15,18 @@ class UserSerializer(serializers.ModelSerializer):
         return obj.email
 
 class CommentSerializer(serializers.ModelSerializer):
-    author_email = serializers.EmailField(source='author.email', read_only=True)
+    author = serializers.SerializerMethodField()
+    content = serializers.CharField(source='text')
 
     class Meta:
         model = Comment
-        fields = ['id', 'task', 'author', 'author_email', 'text', 'created_at']
-        read_only_fields = ['id', 'task', 'author', 'author_email', 'created_at']
+        fields = ['id', 'created_at', 'author', 'content']
 
-# Für Detail-Ansicht einzelner Tasks inkl. Kommentare
+    def get_author(self, obj):
+        if obj.author:
+            return getattr(obj.author, "full_name", obj.author.email)
+        return "Unbekannt"
+
 class TaskSerializer(serializers.ModelSerializer):
     status_display = serializers.CharField(source='get_status_display', read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
@@ -44,7 +48,6 @@ class TaskSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id', 'created_by', 'created_at', 'comments']
 
-# Für Task-Listen-APIs (assigned-to-me, reviewing, Board-Detail)
 class TaskListSerializer(serializers.ModelSerializer):
     assignee = UserSerializer(read_only=True)
     reviewer = UserSerializer(read_only=True)
@@ -62,7 +65,6 @@ class TaskListSerializer(serializers.ModelSerializer):
 
 class BoardSerializer(serializers.ModelSerializer):
     members = UserSerializer(many=True, read_only=True)
-    # Für Board-Detail (tasks-Array) verwende TaskListSerializer!
     tasks = TaskListSerializer(many=True, read_only=True)
     owner_id = serializers.IntegerField(source='owner.id', read_only=True)
 
