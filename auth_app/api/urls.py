7f8ca2b806b745617e45ce3d741c2b3cdff49ac7
Leugzim Rullani <leugzimrullani@outlook.com>
2025-06-15 17:16:49 +0200

    Add: Clean API endpoints for registration, login, and email-check

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import RegistrationView, LoginView, EmailCheckView
 
+# API endpoints for authentication (registration, login, email check).
 urlpatterns = [
     path("registration/", RegistrationView.as_view(), name="registration"),
     path("login/", LoginView.as_view(), name="login"),
     path(
         "email-check/", EmailCheckView.as_view(), name="email-check"
-    ),  # <--- NEU
+    ),
 ]
